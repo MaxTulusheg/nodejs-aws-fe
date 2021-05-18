72e570e2c44e8d7a3776599b3ed066bfdd62f2a3
Maksym Tulusha <Maksym_Tulusha@epam.com>
2021-05-18 13:46:42 +0300

    Added path for import API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://kmvp29m72b.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://o0eeyzm5j1.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://kmvp29m72b.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
