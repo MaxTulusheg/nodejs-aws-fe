c7f64f5475ad48ff38d8294a60b887ca1c915aff
Maksym Tulusha <Maksym_Tulusha@epam.com>
2021-07-05 19:01:48 +0300

    Added url for Cart API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://o0eeyzm5j1.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://kmvp29m72b.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://node-aws-rs-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
